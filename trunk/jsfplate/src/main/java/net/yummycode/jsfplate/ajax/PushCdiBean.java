b06acf750b2d8ec885e45bc03c4f47c89998e4f5
fandry <fandry@92fa828c-20e4-11df-a5a4-5bcf8f6a2e7e>
2013-03-21 22:27:54 +0000

    Fix Ajax-CDI-Push for JBOSS 7.X and Richfaces 4.3.1

--- trunk/jsfplate/src/main/java/net/yummycode/jsfplate/ajax/PushCdiBean.java
+++ trunk/jsfplate/src/main/java/net/yummycode/jsfplate/ajax/PushCdiBean.java
@@ -1,53 +1,28 @@
 package net.yummycode.jsfplate.ajax;
 
 import java.io.Serializable;
-import java.util.UUID;
 
-import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
 import javax.enterprise.event.Event;
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
 
 import javax.inject.Inject;
+import javax.inject.Named;
 
-import org.richfaces.application.push.TopicKey;
-import org.richfaces.application.push.TopicsContext;
 import org.richfaces.cdi.push.Push;
 
-@SessionScoped
-@ManagedBean(name = "pushCdiBean")
+@Named
+@RequestScoped
 public class PushCdiBean implements Serializable {
 
 	private static final long serialVersionUID = 6414191802542861042L;
-	private String userIdentifier;
 	public static final String PUSH_CDI_TOPIC = "pushCdi";
 	private String message = "";
 
 	@Inject
-	@Push(topic = PUSH_CDI_TOPIC, subtopic = "#{pushCdiBean.userIdentifier}")
+	@Push(topic = PUSH_CDI_TOPIC)
 	Event<String> pushEvent;
 	
-	private transient TopicKey topicKey;     
-	private transient TopicsContext topicsContext;      
 	
-	@PostConstruct
-	public void init() {
-
-		System.out.println("*** init()...");
-
-		if (userIdentifier == null) {
-			userIdentifier = UUID.randomUUID().toString().replace("-", "");
-		}
-
-		try {
-			topicsContext = TopicsContext.lookup();
-			topicKey = new TopicKey(PUSH_CDI_TOPIC, userIdentifier);
-			topicsContext.getOrCreateTopic(topicKey);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
 	public void sendMessage() throws Exception {
 		if (pushEvent == null) {
 			System.out.println("*** pushEvent = null!");
@@ -65,11 +40,4 @@ public class PushCdiBean implements Serializable {
 		this.message = message;
 	}
 
-	public String getUserIdentifier() {
-		return userIdentifier;
-	}
-
-	public void setUserIdentifier(String userIdentifier) {
-		this.userIdentifier = userIdentifier;
-	}
 }
